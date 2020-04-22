132dbbe269f388c95f3a9329dfff727cf87d7d2f
l1997i@yeah.net <l1997i@yeah.net>
2020-04-22 16:51:23 +0800

    add testBoggleBoard() func

--- 4_Boggle/TestSolver.java
+++ 4_Boggle/TestSolver.java
@@ -1,5 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
-
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -7,13 +7,39 @@ public class TestSolver {
 
     @Test
     public void testBoggleSolver() {
+        printHeader("testBoggleSolver");
         In in = new In("data/dictionary-shakespeare.txt");
         String[] dictionary = in.readAllStrings();
         BoggleSolver solver = new BoggleSolver(dictionary);
         BoggleBoard board = new BoggleBoard();
-        for (String s:solver.getAllValidWords(board)){
+        for (String s : solver.getAllValidWords(board)) {
             StdOut.println(s);
         }
-        StdOut.print("test success!");
+        printFooter("testBoggleSolver");
+    }
+
+    @Test
+    public void testBoggleBoard() {
+        // Familiarize myself with the BoggleBoard.java data type
+        printHeader("testBoggleBoard");
+        BoggleBoard myBoard = new BoggleBoard("data/board4x4.txt");
+        int cols = myBoard.cols();
+        int rows = myBoard.rows();
+        StdOut.println("cols: " + cols);
+        StdOut.println("rows: " + rows);
+        assertEquals(4, cols, "cols not equal!");
+        assertEquals(4, rows, "rows not equal!");
+        char boardLetter = myBoard.getLetter(2, 2);
+        StdOut.println("getLetter(2, 2): " + boardLetter);
+        assertEquals('N', boardLetter, "Letter in (2,2) not equal!");
+        printFooter("testBoggleBoard");
+    }
+
+    private void printHeader(String funcName) {
+        StdOut.println("=========== " + funcName + " begins ===========");
+    }
+
+    private void printFooter(String funcName) {
+        StdOut.println("=> " + funcName + " complete.");
     }
 }
