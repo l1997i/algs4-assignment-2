798347705f2f04d840fc7b7baee5df23b9c369c7
l1997i@yeah.net <l1997i@yeah.net>
2020-04-29 21:57:47 +0800

    Initial files

---
+++ 5_Burrows/MoveToFront.java
@@ -0,0 +1,37 @@
+/**
+ * Move-to-front encoding. Given a text file in which sequences of the same
+ * character occur near each other many times, convert it into a text file in
+ * which certain characters appear much more frequently than others.
+ * 
+ * @author Li Li
+ * @since Apr. 29, 2020
+ */
+public class MoveToFront {
+
+    /**
+     * apply move-to-front encoding, reading from standard input and writing to
+     * standard output
+     */
+    public static void encode() {
+
+    }
+
+    /**
+     * apply move-to-front decoding, reading from standard input and writing to
+     * standard output
+     */
+    public static void decode() {
+
+    }
+
+    /**
+     * if args[0] is "-", apply move-to-front encoding if args[0] is "+", apply
+     * move-to-front decoding
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
